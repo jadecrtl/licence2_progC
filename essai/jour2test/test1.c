2c518c2dfa3a7ac9ddcd3e5e891e0835636deada
jadecrtl <jadecortial@gmail.com>
2022-01-24 09:39:04 +0100

    scanf

--- essai/jour2test/test1.c
+++ essai/jour2test/test1.c
@@ -2,17 +2,18 @@
 
 int main() {
     int a;
+    printf("Entrez un nombre : ");
     scanf("%d", &a); //analyse l'entree qui corres aux specificateurs de format
     /*
     Le signe & avant le nom de la variable est l'opérateur d'adresse. Il donne l'adresse ou l'emplacement en memoire d'une variable. Ceci est nécessaire car scanf place une valeur d'entree à une adresse variable
     */
-    printf("You entered: %d", a);
+    printf("Vous avez entre ce nombre : %d", a);
 
-    int b;
+    int c, b;
     printf("\nEnter two numbers:");
-    scanf("%d %d", &a, &b);
+    scanf("%d %d", &b, &c);
     
-    printf("\nSum: %d", a+b);
+    printf("\nSum: %d\n", b+c);
 
     return 0;
 }
